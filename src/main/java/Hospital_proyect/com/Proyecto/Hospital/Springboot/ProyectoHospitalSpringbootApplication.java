d6d1996a0f3ee165c61bcc88ce6102df943c4b9b
Njco1 <Nicolasaf17@gmail.com>
2025-01-13 12:52:52 -0500

    Initial commit

---
+++ src/main/java/Hospital_proyect/com/Proyecto/Hospital/Springboot/ProyectoHospitalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package Hospital_proyect.com.Proyecto.Hospital.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHospitalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHospitalSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hospital_proyect/com/Proyecto/Hospital/Springboot/ProyectoHospitalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package Hospital_proyect.com.Proyecto.Hospital.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHospitalSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
